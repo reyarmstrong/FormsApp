fc684884ce18523b81cdb19fda943f288c6384e7
reyarmstrong <reylevig2@hotmail.com>
2025-01-18 14:56:03 -0600

    wip.

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -83,5 +83,7 @@ export class DynamicPageComponent {
     }
 
     console.log(this.myForm.value);
+    (this.myForm.controls['favoriteGames'] as FormArray) = this.fb.array([]);
+    this.myForm.reset();
   }
 }
