72b5401bbd7ff1c823fe5b17186330fda6f1a70c
reyarmstrong <reylevig2@hotmail.com>
2025-01-18 14:34:21 -0600

    Add dynamic validations.

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   standalone: false,
-  
+
   templateUrl: './dynamic-page.component.html',
   styles: ``
 })
 export class DynamicPageComponent {
 
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      name: ['', [ Validators.required, Validators.minLength(3) ]],
+      favoriteGames: this.fb.array([
+        ['Metal Gear', Validators.required],
+        ['Death Stranding', Validators.required]
+      ])
+    });
+  }
+
+  get favoriteGames(): FormArray {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  onSubmit(): void {
+
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+  }
 }
